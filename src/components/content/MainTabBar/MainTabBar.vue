06cc3f091519aa528f93b2891ede1212160ef2ce
SavitarXiangzi <1499113125@qq.com>
2020-09-10 17:49:49 +0800

    test

--- src/components/content/MainTabBar/MainTabBar.vue
+++ src/components/content/MainTabBar/MainTabBar.vue
@@ -2,40 +2,40 @@
   <tab-bar>
     <tab-bar-item link="/home" activeColor="pink">
       <template v-slot:item-icon>
-        <img  src="../../../assets/img/tabbar/home.svg" alt="" />
+        <img  src="~assets/img/tabbar/home.svg" alt="" />
       </template>
       <template v-slot:item-icon-active>
-        <img  src="../../../assets/img/tabbar/home_active.svg" alt=""/>
+        <img  src="~assets/img/tabbar/home_active.svg" alt=""/>
       </template>
       <template v-slot:item-text>首页</template>
     </tab-bar-item>
 
     <tab-bar-item link="/category" activeColor="pink">
       <template v-slot:item-icon>
-        <img  src="../../../assets/img/tabbar/category.svg" alt="" />
+        <img  src="~assets/img/tabbar/category.svg" alt="" />
       </template>
       <template v-slot:item-icon-active>
-        <img  src="../../../assets/img/tabbar/category_active.svg" alt=""/>
+        <img  src="~assets/img/tabbar/category_active.svg" alt=""/>
       </template>
       <template v-slot:item-text>首页</template>
     </tab-bar-item>
 
     <tab-bar-item link="/cart" activeColor="pink">
       <template v-slot:item-icon>
-        <img  src="../../../assets/img/tabbar/cart.svg" alt="" />
+        <img src="~assets/img/tabbar/cart.svg" alt="" />
       </template>
       <template v-slot:item-icon-active>
-        <img  src="../../../assets/img/tabbar/cart_active.svg" alt=""/>
+        <img src="~assets/img/tabbar/cart_active.svg" alt=""/>
       </template>
       <template v-slot:item-text>首页</template>
     </tab-bar-item>
 
     <tab-bar-item link="/profile" activeColor="pink">
       <template v-slot:item-icon>
-        <img  src="../../../assets/img/tabbar/profile.svg" alt="" />
+        <img  src="~assets/img/tabbar/profile.svg" alt="" />
       </template>
       <template v-slot:item-icon-active>
-        <img  src="../../../assets/img/tabbar/profile_active.svg" alt=""/>
+        <img  src="~assets/img/tabbar/profile_active.svg" alt=""/>
       </template>
       <template v-slot:item-text>首页</template>
     </tab-bar-item>
@@ -43,8 +43,8 @@
 </template>
 
 <script>
-import TabBar from '../../tabbar/TabBar'
-import TabBarItem from '../../tabbar/TabBarItem'
+import TabBar from 'components/common/tabbar/TabBar'
+import TabBarItem from 'components/common/tabbar/TabBarItem'
 
 export default {
   name: "MainTabBar",
